ad92d8c539a8d50b227f3ca98fb124ff00e432c7
CorellCB <corellcb@gmail.com>
2019-11-12 17:24:14 -0500

    completed getRunnersByTShirtSize

--- index.js
+++ index.js
@@ -204,8 +204,12 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const nameDrop = function (element) {
+    return element.first_name.toUpperCase();
+  }
+  
+  return runners.map(nameDrop);
 }
 
 /**
@@ -221,8 +225,12 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  let sizeCheck = function(element) {
+    return element.shirt_size === tShirtSize;
+  }
+  
+  return runners.filter(sizeCheck);
 }
 
 /**
